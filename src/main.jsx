9e1b01a140e65e1b0274b0c51ef54b6103908990
FTTM-M <fatemehalimirzaee14@gmail.com>
2026-09-21 22:10:31 +0330

    wrote the slice codes

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 import "./global.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
-    <BrowserRouter>
+  <BrowserRouter>
+    {" "}
+    <Provider store={store}>
       {" "}
       <App />
-    </BrowserRouter>
+    </Provider>
+  </BrowserRouter>
   // </StrictMode>,
 );
